d64da32ffb1d3aebd1e7a272941a9aaebb0d5b7b
Mariusz Gil <mariusz.gil91@gmail.com>
2021-01-14 18:57:11 +0100

    create/delete game

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,5 +1,6 @@
 import static io.restassured.matcher.RestAssuredMatchers.matchesXsdInClasspath;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.lessThan;
 
 import config.VideoGameAPIConfig;
@@ -11,9 +12,44 @@ import static io.restassured.RestAssured.*;
 
 public class FirstTest extends VideoGameAPIConfig {
     @Test
-    public void getAllGames() {
+    public void getAllVideoGames() {
         given().
-                when().get(VideoGameAPIEndpoints.ALL_VIDEO_GAMES).
-                then();
+        when().
+                get(VideoGameAPIEndpoints.ALL_VIDEO_GAMES).
+        then();
+    }
+
+    @Test
+    public void createGame11JSON(){
+
+        String gameJSON = "{\n" +
+                "    \"id\": 11,\n" +
+                "    \"name\": \"Bloodborne\",\n" +
+                "    \"releaseDate\": \"2021-01-14T16:59:51.198Z\",\n" +
+                "    \"reviewScore\": 99,\n" +
+                "    \"category\": \"SoulsLike\",\n" +
+                "    \"rating\": \"Awesome\"\n" +
+                "  }\n";
+
+        given().
+                body(gameJSON).
+        when().
+                post(VideoGameAPIEndpoints.ALL_VIDEO_GAMES).
+        then().
+                body("id", equalTo("11")).
+                body("name", equalTo("Bloodborne")).
+                body("releaseDate", equalTo("2021-01-14T16:59:51.198Z")).
+                body("reviewScore", equalTo("99")).
+                body("category", equalTo("SoulsLike")).
+                body("rating", equalTo("Awesome"));
+    }
+
+    @Test
+    public void deleteGame11JSON(){
+        given().
+
+        when().
+                delete(VideoGameAPIEndpoints.ALL_VIDEO_GAMES + "/11").
+        then();
     }
 }
--- src/test/java/config/VideoGameAPIEndpoints.java
+++ src/test/java/config/VideoGameAPIEndpoints.java
@@ -2,4 +2,5 @@ package config;
 
 public interface VideoGameAPIEndpoints {
     String ALL_VIDEO_GAMES = "videogames";
+    String ID_VIDEO_GAMES = "videogames/videoGameId";
 }
